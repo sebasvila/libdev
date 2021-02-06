1cfbad08881dc83d0556970b40496e1831ece2c0
Pau Lafoz <pau@lacodornella.cat>
2021-02-06 19:23:46 +0100

    LCD I2C module implemented.

--- src/lcd_i2c.c
+++ src/lcd_i2c.c
@@ -1,11 +1,59 @@
-#include <avr/io.h>
 #include <stdbool.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <util/delay.h>
-#include <i2c.h>
+#include <util/atomic.h>
 #include "lcd_i2c.h"
 
+
+//I2C Expander pins
+#define LCD_RS_PIN          0x01
+#define LCD_RW_PIN          0x02
+#define LCD_EN_PIN          0x04
+#define LCD_BACKLIGHT_PIN   0x08
+
+//Main instructions (page 24)
+#define LCD_CLEARDISPLAY   0x01
+
+#define LCD_RETURNHOME     0x02
+
+#define LCD_ENTRYMODESET   0x04 
+  #define LCD_ENTRYRIGHT          0x00
+  #define LCD_ENTRYLEFT           0x02
+  #define LCD_ENTRYSHIFTINCREMENT 0x01
+  #define LCD_ENTRYSHIFTDECREMENT 0x00  
+
+#define LCD_DISPLAYCONTROL 0x08
+  #define LCD_DISPLAYON  0x04
+  #define LCD_DISPLAYOFF 0x00
+  #define LCD_CURSORON   0x02
+  #define LCD_CURSOROFF  0x00
+  #define LCD_BLINKON    0x01
+  #define LCD_BLINKOFF   0x00
+
+#define LCD_CURSORSHIFT    0x10
+  #define LCD_DISPLAYMOVE 0x08
+  #define LCD_CURSORMOVE  0x00
+  #define LCD_MOVERIGHT   0x04
+  #define LCD_MOVELEFT    0x00
+
+#define LCD_FUNCTIONSET    0x20
+  #define LCD_8BITMODE  0x10
+  #define LCD_4BITMODE  0x00
+  #define LCD_2LINE     0x08
+  #define LCD_1LINE     0x00
+  #define LCD_5x10DOTS  0x04
+  #define LCD_5x8DOTS   0x00
+
+#define LCD_SETCGRAMADDR   0x40
+#define LCD_SETDDRAMADDR   0x80
+
+/**
+ * @brief Defines the two different types of messages to the display
+ */
+typedef enum {
+  COMMAND = 0x00,
+  DATA = 0x01
+} rs_mode_t;
 
 
 lcd_t lcd_constructor(const uint8_t i2c_address, const uint8_t rows){
@@ -15,300 +63,223 @@ lcd_t lcd_constructor(const uint8_t i2c_address, const uint8_t rows){
     0,            //Function
     0,            //Params
     0,            //Mode
+    Success,      //Last I2C request status
+    0xFF,         //Recieve buffer
   };
   return l;
 }
 
 
-static void lcd_write_nibble(const lcd_t *l, uint8_t nibble) {
+static void lcd_write_nibble(lcd_t *l, uint8_t nibble, volatile i2c_status_t * st) {
   while (i2c_swamped());
   i2c_send_uint8(l->i2c_address,
                   nibble | LCD_EN_PIN, 
                   NULL);
-
   while (i2c_swamped());
   i2c_send_uint8(l->i2c_address,
                   nibble & ~LCD_EN_PIN,
-                  NULL);
+                  st);
 }
 
 
 /**
- * Sends a DATA or COMMAND byte instruction to the LCD.  Internally,
- * as the display is connected using 4-bit data bus over I2c, the function
- * splits the byte into two nibble-sized messages.
+ * @brief Sends a DATA or COMMAND byte instruction to the LCD.
+ * Internally, as the display is connected using 4-bit data bus over I2c,
+ * the function splits the byte into two nibble-sized messages.
  *
- * @param l The LCD object where the message must be adressed
- * @param rs_mode Message is a LCD_COMMAND LCD_DATA
+ * @param l Pointer to the LCD object where the message must be adressed
+ * @param rs Message is a `COMMAND` or `DATA`
  * @param message The byte to be sent
  */
-void lcd_send(const lcd_t *l, const uint8_t rs_mode, uint8_t message) {
-  uint8_t nibble = (rs_mode | LCD_BACKLIGHT_PIN) & 0x0F;
+static void lcd_send(lcd_t *l, const rs_mode_t rs, uint8_t message) {
+  while(l->i2c_comm == Running);  //If LCD is still pending on the last i2c command, function will block.
+  
+  uint8_t nibble = (rs | LCD_BACKLIGHT_PIN) & 0x0F;
   
   //High nibble
-  lcd_write_nibble(l, (message & 0xF0) | nibble);
+  lcd_write_nibble(l, (message & 0xF0) | nibble, NULL);
   
   //Low nibble
-  lcd_write_nibble(l, ((message << 4) & 0xF0) | nibble);
+  lcd_write_nibble(l, ((message << 4) & 0xF0) | nibble, &l->i2c_comm);
+}
+
+
+void lcd_busy_flag_request(lcd_t *l){
+  if (l->i2c_comm == Running){
+    return;
+  }  //If LCD is still pending on the last i2c command, function returns.
+
+  //High nibble on HIGH to be able to read D7-D4 pins
+  const uint8_t s_buffer = 0xF0 | COMMAND | LCD_BACKLIGHT_PIN | LCD_RW_PIN;
+
+  i2c_send_uint8(l->i2c_address, s_buffer, NULL);                 //High nibble (D7-D4) on HIGH to be able to read it (PCF8574 requirement)
+  i2c_send_uint8(l->i2c_address, s_buffer | LCD_EN_PIN, NULL);    //Rising edge ENABLE pulse
+  i2c_receive_uint8(l->i2c_address, &l->r_buffer, &l->i2c_comm);  //Receive byte I2C request. Busy flag will be stored at the MSB on `r_buffer`
+  i2c_send_uint8(l->i2c_address, s_buffer, NULL);                 //Falling edge ENABLE pulse
+
+  //2nd ENABLE pulse is needed as LCD is working in 4-bit mode.
+  i2c_send_uint8(l->i2c_address, s_buffer | LCD_EN_PIN, NULL);    //Rising edge ENABLE pulse
+  i2c_send_uint8(l->i2c_address, s_buffer, NULL);                 //Falling edge ENABLE pulse
+}
+
+
+bool lcd_is_busy_flag_set(lcd_t *l){
+  bool busy_flag = true;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+    if (l->i2c_comm == Success && !(l->r_buffer >> 7)) {                      //BF is cleared
+      //Received buffer to 0xFF to reset the internal BF bit
+      l->r_buffer = 0xFF;
+      busy_flag = false;
+    }
+  }
+  return busy_flag;
 }
 
 
 void lcd_init(lcd_t *l){
-	l->function = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
+	//The following seqüence is according to the Hitachi HD44780 datasheet (page 46)
+	//to init the LCD into 4 bit mode
 
+	lcd_write_nibble(l, 0x03 << 4, NULL);
+	_delay_ms(5); // Wait min 4.1ms
+
+	lcd_write_nibble(l, 0x03 << 4, NULL);
+	_delay_ms(5); // Wait min 4.1ms
+
+	lcd_write_nibble(l, 0x03 << 4, NULL);
+	_delay_us(150);
+
+	//Finally, set to 4-bit interface
+	lcd_write_nibble(l, 0x02 << 4, NULL);
+
+	//Set # lines, font size, etc.
+  l->function = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
 	if (l->rows > 1) {
 		l->function |= LCD_2LINE;
 	}
+  lcd_send(l, COMMAND, LCD_FUNCTIONSET | l->function);
 
-	// SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
-	// according to datasheet, we need at least 40ms after power rises above 2.7V
-	// before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
-	_delay_ms(50);
-
-	// Now we pull both RS and R/W low to begin commands
-	i2c_send_uint8(l->i2c_address, LCD_BACKLIGHT_PIN, NULL);
-	_delay_ms(1000);
-
-	//put the LCD into 4 bit mode
-	// this is according to the hitachi HD44780 datasheet
-	// figure 24, pg 46
-
-	// we start in 8bit mode, try to set 4 bit mode
-	lcd_write_nibble(l, 0x03 << 4);
-	_delay_ms(5); // wait min 4.1ms
-
-	// second try
-	lcd_write_nibble(l, 0x03 << 4);
-	_delay_ms(5); // wait min 4.1ms
-
-	// third go!
-	lcd_write_nibble(l, 0x03 << 4);
-	_delay_us(150);
-
-	// finally, set to 4-bit interface
-	lcd_write_nibble(l, 0x02 << 4);
-
-	// set # lines, font size, etc.
-  lcd_send(l, LCD_COMMAND, LCD_FUNCTIONSET | l->function);
-
-	// turn the display on with no cursor or blinking default
+	//Turn the display on with no cursor or blinking default
 	l->params = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
-  lcd_send(l, LCD_COMMAND, LCD_DISPLAYCONTROL | l->params);
+  lcd_send(l, COMMAND, LCD_DISPLAYCONTROL | l->params);
 
-  lcd_clear(l);
-
-	// Initialize to default text direction (for roman languages)
+	//Set the entry mode. Initialize to default text direction (for roman languages)
 	l->mode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+	lcd_send(l, COMMAND, LCD_ENTRYMODESET | l->mode);
 
-	// set the entry mode
-	lcd_send(l, LCD_COMMAND, LCD_ENTRYMODESET | l->mode);
+  lcd_clear(l, true);
 
-	lcd_return_home(l);
+	lcd_return_home(l, true);
 }
 
 
-/*
- * Turns the LCD ON
- * @param l The LCD object to turn ON
- */
+void lcd_return_home(lcd_t *l, const bool blocking) {
+  lcd_send(l, COMMAND, LCD_RETURNHOME);
+  if (blocking){
+    _delay_ms(2);
+  }
+}
+
+
 void lcd_on(lcd_t *l) {
   l->params |= LCD_DISPLAYON;
-  lcd_send(l, LCD_COMMAND, LCD_DISPLAYCONTROL | l->params);
+  lcd_send(l, COMMAND, LCD_DISPLAYCONTROL | l->params);
 }
 
 
-/*
- * Turns the LCD OFF
- * @param l The LCD object
- */
 void lcd_off(lcd_t *l) {
   l->params &= ~LCD_DISPLAYON;
-  lcd_send(l, LCD_COMMAND, LCD_DISPLAYCONTROL | l->params);
+  lcd_send(l, COMMAND, LCD_DISPLAYCONTROL | l->params);
 }
 
 
-/*
- * Clears the LCD display. The function takes 2ms (blocking)
- * @param l The LCD object that must be cleared
- */
-void lcd_clear(const lcd_t *l) {
-  lcd_send(l, LCD_COMMAND, LCD_CLEARDISPLAY);
-  _delay_ms(2);   //Atention!
+void lcd_clear(lcd_t *l, const bool blocking) {
+  lcd_send(l, COMMAND, LCD_CLEARDISPLAY);
+  if (blocking){
+    _delay_ms(2);
+  }
 }
 
 
-
-
-/*
- * Returns both display and cursor to the original 
- * position (address 0). The function takes 2ms (blocking)
- * @param l The LCD object
- */
-void lcd_return_home(const lcd_t *l) {
-  lcd_send(l, LCD_COMMAND, LCD_RETURNHOME);
-  _delay_ms(2); //Atention
-}
-
-
-/*
- * Enables the cursor blinking
- * @param l The LCD object
- */
 void lcd_enable_blinking(lcd_t *l) {
   l->params |= LCD_BLINKON;
-  lcd_send(l, LCD_COMMAND, LCD_DISPLAYCONTROL | l->params);
+  lcd_send(l, COMMAND, LCD_DISPLAYCONTROL | l->params);
 }
 
 
-/*
- * Disables the cursor blinking
- * @param l The LCD object
- */
 void lcd_disable_blinking(lcd_t *l) {
   l->params &= ~LCD_BLINKON;
-  lcd_send(l,LCD_COMMAND, LCD_DISPLAYCONTROL | l->params);
+  lcd_send(l,COMMAND, LCD_DISPLAYCONTROL | l->params);
 }
 
 
-/*
- * Enables the cursor visibility
- * @param l The LCD object
- */
 void lcd_enable_cursor(lcd_t *l) {
   l->params |= LCD_CURSORON;
-  lcd_send(l, LCD_COMMAND, LCD_DISPLAYCONTROL | l->params);
+  lcd_send(l, COMMAND, LCD_DISPLAYCONTROL | l->params);
 }
 
 
-/*
- * Disables the cursor visibility
- * @param l The LCD object
- */
 void lcd_disable_cursor(lcd_t *l) {
   l->params &= ~LCD_CURSORON;
-  lcd_send(l, LCD_COMMAND, LCD_DISPLAYCONTROL | l->params);
+  lcd_send(l, COMMAND, LCD_DISPLAYCONTROL | l->params);
 }
 
 
-/*
- * Sets the scroll direction to LEFT
- * @param l The LCD object
- */
-void lcd_scroll_left(const lcd_t *l) {
-  lcd_send(l, LCD_COMMAND,  LCD_CURSORSHIFT |
+void lcd_scroll_left(lcd_t *l) {
+  lcd_send(l, COMMAND,  LCD_CURSORSHIFT |
                             LCD_DISPLAYMOVE |
 	                          LCD_MOVELEFT);
 }
 
 
-/*
- * Sets the scroll direction to RIGHT
- * @param l The LCD object
- */
-void lcd_scroll_right(const lcd_t *l) {
-  lcd_send(l, LCD_COMMAND,  LCD_CURSORSHIFT |
+void lcd_scroll_right(lcd_t *l) {
+  lcd_send(l, COMMAND,  LCD_CURSORSHIFT |
 	                          LCD_DISPLAYMOVE | 
                             LCD_MOVERIGHT);
 }
 
 
-/*
- * Sets the entry mode to be left to right. 
- * Means the cursor will move RIGHT at each written character.
- * @param l The LCD object
- */
 void lcd_set_left_to_right(lcd_t *l) {
   l->params |= LCD_ENTRYLEFT;
-  lcd_send(l, LCD_COMMAND, LCD_ENTRYMODESET | l->params);
+  lcd_send(l, COMMAND, LCD_ENTRYMODESET | l->params);
 }
 
 
-/*
- * Sets the entry mode to be right to left. Means the cursor 
- * will move LEFT at each written character.
- * @param l The LCD object
- */
 void lcd_set_right_to_left(lcd_t *l) {
   l->params &= ~LCD_ENTRYLEFT;
-  lcd_send(l, LCD_COMMAND, LCD_ENTRYMODESET | l->params);
+  lcd_send(l, COMMAND, LCD_ENTRYMODESET | l->params);
 }
 
 
-/*
- * Enables the autoscroll mode.
- * @param l The LCD object
- */
 void lcd_enable_autoscroll(lcd_t *l) {
   l->params |= LCD_ENTRYSHIFTINCREMENT;
-  lcd_send(l, LCD_COMMAND, LCD_ENTRYMODESET | l->params);
+  lcd_send(l, COMMAND, LCD_ENTRYMODESET | l->params);
 }
 
 
-/*
- * Disables the autoscroll mode.
- * @param l The LCD object
- */
 void lcd_disable_autoscroll(lcd_t *l) {
   l->params &= ~LCD_ENTRYSHIFTINCREMENT;
-  lcd_send(l, LCD_COMMAND, LCD_ENTRYMODESET | l->params);
+  lcd_send(l, COMMAND, LCD_ENTRYMODESET | l->params);
 }
 
 
-/*
- * Creates a new character. The LCD can save up to 8 
- * user-defined characters (5x8px). 
- * The lcd_move_cursor() must be called after the char 
- * creation, otherwise the cursor is pointing somewhere else.
- *
- * @param l The LCD object
- * @param location The address to save the new character.
- * @param charmap The charmap containing the new char information
- */
-void lcd_create_char(const lcd_t *l, uint8_t location, uint8_t *charmap) {
-  lcd_send(l, LCD_COMMAND, (LCD_SETCGRAMADDR | ((location & 0x07) << 3)));
+void lcd_create_char(lcd_t *l, uint8_t location, uint8_t *charmap) {
+  lcd_send(l, COMMAND, (LCD_SETCGRAMADDR | ((location & 0x07) << 3)));
   for (int i = 0; i < 8; i++) {
-    lcd_send(l, LCD_DATA, *charmap);
+    lcd_send(l, DATA, *charmap);
     charmap++;
   }
 }
 
 
-/*
- * Move the LCD cursor to the desired position
- * @param l The LCD object
- * @param col The column
- * @param row The row
- */
-void lcd_move_cursor(const lcd_t *l, uint8_t col, uint8_t row) {
+void lcd_move_cursor(lcd_t *l, uint8_t col, uint8_t row) {
   static uint8_t offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-  lcd_send(l, LCD_COMMAND, LCD_SETDDRAMADDR | (col + offsets[row]));
+  lcd_send(l, COMMAND, LCD_SETDDRAMADDR | (col + offsets[row]));
 }
 
 
-/*
- * Sends a string to the LCD
- * @param l The LCD object to send the string
- * @param string The string object to be sent
- */
-void lcd_print(const lcd_t *l, char *string) {
+void lcd_print(lcd_t *l, char *string) {
   for (char *it = string; *it; it++) {
-    lcd_send(l, LCD_DATA, *it);
+    lcd_send(l, DATA, *it);
   }
-}
-
-
-/*
- * Prints a formated string on the LCD
- * @param l The LCD object
- * @param format The
- */
-void lcd_printf(const lcd_t *l, char *format, ...) {
-  va_list args;
-  char lcd_buffer[LCD_COL_COUNT + 1];
-
-  va_start(args, format);
-  vsnprintf(lcd_buffer, LCD_COL_COUNT + 1, format, args);
-  va_end(args);
-
-  lcd_print(l, lcd_buffer);
 }
--- src/lcd_i2c.h
+++ src/lcd_i2c.h
@@ -1,60 +1,12 @@
 #ifndef LCD_I2C_H
 #define LCD_I2C_H
 
-#pragma once
-
-#include <avr/io.h>
 #include <stdbool.h>
+#include <i2c.h>
 
 /* A 'lcd_t' is an abstraction of an LCD display using the HD44780
  * controller through a PCF8574 I2C expander.
  */
-
-#define LCD_COL_COUNT 20
-#define LCD_ROW_COUNT 4   //Not used
-
-// The rest should be left alone
-#define LCD_CLEARDISPLAY   0x01
-#define LCD_RETURNHOME     0x02
-#define LCD_ENTRYMODESET   0x04
-#define LCD_DISPLAYCONTROL 0x08
-#define LCD_CURSORSHIFT    0x10
-#define LCD_FUNCTIONSET    0x20
-#define LCD_SETCGRAMADDR   0x40
-#define LCD_SETDDRAMADDR   0x80
-
-#define LCD_ENTRYRIGHT          0x00
-#define LCD_ENTRYLEFT           0x02
-#define LCD_ENTRYSHIFTINCREMENT 0x01
-#define LCD_ENTRYSHIFTDECREMENT 0x00  
-
-#define LCD_DISPLAYON  0x04
-#define LCD_DISPLAYOFF 0x00
-#define LCD_CURSORON   0x02
-#define LCD_CURSOROFF  0x00
-#define LCD_BLINKON    0x01
-#define LCD_BLINKOFF   0x00
-
-#define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE  0x00
-#define LCD_MOVERIGHT   0x04
-#define LCD_MOVELEFT    0x00
-
-#define LCD_8BITMODE  0x10
-#define LCD_4BITMODE  0x00
-#define LCD_2LINE     0x08
-#define LCD_1LINE     0x00
-#define LCD_5x10DOTS  0x04
-#define LCD_5x8DOTS   0x00
-
-#define LCD_COMMAND   0x00
-#define LCD_DATA      0x01
-
-//I2C Expander pins
-#define LCD_RS_PIN          0x01
-#define LCD_RW_PIN          0x02
-#define LCD_EN_PIN          0x04
-#define LCD_BACKLIGHT_PIN   0x08
 
 typedef struct {
   const uint8_t i2c_address;
@@ -62,6 +14,8 @@ typedef struct {
   uint8_t function;
   uint8_t params;
   uint8_t mode;
+  volatile i2c_status_t i2c_comm;
+  uint8_t r_buffer;
 } lcd_t;
 
 /**
@@ -81,47 +35,172 @@ typedef struct {
  * | H  | H  | H  |   0x27  |   0x3F   |
  * 
  * @param i2c_address The I2C node address. 
+ * @param rows Number of rows of the display.
  * @return lcd_t The LCD I2C object.
  */
 lcd_t lcd_constructor(const uint8_t i2c_address, const uint8_t rows);
 
 
 /**
- * @brief Initializes the LCD. Must be called after the 'i2c' module has been initialized.
+ * @brief Initializes the LCD.
+ * Must be called after the 'i2c' module has been initialized.
+ * According to page 45/46 from datasheet, initialitzation should be started
+ * at least 40ms after Vcc rises 2.7V. A 50ms delay is recommended between 
+ * power on and LCD init.
  * 
  * @param l The LCD struct variable.
  */
 void lcd_init(lcd_t *l);
 
-void lcd_send(const lcd_t *l, const uint8_t rs_mode, const uint8_t value);
+
+/**
+ * @brief Clears the LCD display.
+ * @param l Pointer to the LCD object
+ * @param blocking `true`: the function blocks during 2ms. `false`: Just sends the command
+ */
+void lcd_clear(lcd_t *l, const bool blocking);
 
 
+/**
+ * @brief Returns both display and cursor to the first 
+ * position (address 0).
+ * @param l Pointer to the LCD object
+ * @param blocking `true`: the function will block during 2ms. `false`: Just sends the command.
+ */
+void lcd_return_home(lcd_t *l, const bool blocking);
+
+
+/**
+ * @brief Request the LCD the busy flag status.
+ * 
+ * @param l Pointer to the LCD object
+ */
+void lcd_busy_flag_request(lcd_t *l);
+
+
+/**
+ * @brief Check the requested busy flag status.
+ * 
+ * @param l Pointer to the LCD object
+ * @return true if busy flag is still set
+ * @return false if busy flag is cleared 
+ */
+bool lcd_is_busy_flag_set(lcd_t *l);
+
+
+/**
+ * @brief Turns the LCD ON
+ * @param l Pointer to the LCD object
+ */
 void lcd_on(lcd_t *l);
+
+
+/**
+ * @brief Turns the LCD OFF
+ * @param l Pointer to the LCD object
+ */
 void lcd_off(lcd_t *l);
 
-void lcd_clear(const lcd_t *l);
-void lcd_return_home(const lcd_t *l);
 
+/**
+ * @brief Enables the cursor blinking
+ * @param l Pointer to the LCD object
+ */
 void lcd_enable_blinking(lcd_t *l);
+
+
+/**
+ * @brief Disables the cursor blinking
+ * @param l Pointer to the LCD object
+ */
 void lcd_disable_blinking(lcd_t *l);
 
+
+/**
+ * @brief Enables the cursor visibility
+ * @param l The LCD object
+ */
 void lcd_enable_cursor(lcd_t *l);
+
+
+/**
+ * @brief Disables the cursor visibility
+ * @param l Pointer to the LCD object
+ */
 void lcd_disable_cursor(lcd_t *l);
 
-void lcd_scroll_left(const lcd_t *l);
-void lcd_scroll_right(const lcd_t *l);
 
+/**
+ * @brief Sets the scroll direction to LEFT
+ * @param l Pointer to the LCD object
+ */
+void lcd_scroll_left(lcd_t *l);
+
+
+/**
+ * @brief Sets the scroll direction to RIGHT
+ * @param l Pointer to the LCD object
+ */
+void lcd_scroll_right(lcd_t *l);
+
+
+/**
+ * @brief Sets the entry mode to be left to right. 
+ * Means the cursor will move RIGHT at each written character.
+ * @param l Pointer to the LCD object
+ */
 void lcd_set_left_to_right(lcd_t *l);
+
+
+/**
+ * @brief Sets the entry mode to be right to left. Means the cursor 
+ * will move LEFT at each written character.
+ * @param l Pointer to the LCD object
+ */
 void lcd_set_right_to_left(lcd_t *l);
 
+
+/**
+ * @brief Enables the autoscroll mode.
+ * @param l Pointer to the LCD object
+ */
 void lcd_enable_autoscroll(lcd_t *l);
+
+
+/**
+ * @brief Disables the autoscroll mode.
+ * @param l Pointer to the LCD object
+ */
 void lcd_disable_autoscroll(lcd_t *l);
 
-void lcd_create_char(const lcd_t *l, uint8_t location, uint8_t charmap[8]);
 
-void lcd_move_cursor(const lcd_t *l, const uint8_t col, const uint8_t row);
+/**
+ * @brief Creates a new character. The LCD can save up to 8 
+ * user-defined characters (5x8px). 
+ * The lcd_move_cursor() must be called after the char 
+ * creation, otherwise the cursor is pointing somewhere else.
+ *
+ * @param l Pointer to the LCD object
+ * @param location The address to save the new character.
+ * @param charmap The charmap containing the new char information
+ */
+void lcd_create_char(lcd_t *l, uint8_t location, uint8_t charmap[8]);
 
-void lcd_print(const lcd_t *l, char *string);
-void lcd_printf(const lcd_t *l, char *format, ...);
+
+/**
+ * @brief Move the LCD cursor to the desired position
+ * @param l Pointer to the LCD object
+ * @param col The column
+ * @param row The row
+ */
+void lcd_move_cursor(lcd_t *l, const uint8_t col, const uint8_t row);
+
+
+/**
+ * @brief Sends a string to the LCD
+ * @param l Pointer to the LCD object to send the string
+ * @param string The string object to be sent
+ */
+void lcd_print(lcd_t *l, char *string);
 
 #endif
